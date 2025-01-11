dd16ae17787842f24f838b2b13aaa891ea6dde2c
NistonN <nisprogrammer@gmail.com>
2025-01-11 03:55:56 +0300

    comments

--- front-end/modules/ApplicationPostElement/api/api.ts
+++ front-end/modules/ApplicationPostElement/api/api.ts
@@ -1,15 +1,37 @@
 import axios from "axios";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-import { IGetIdApplication } from "../type/type";
+import { IComments, IGetIdApplication } from "../type/type";
 
 export const getIdApplication = async (
 	param: Params
 ): Promise<IGetIdApplication | null> => {
 	try {
-		const response = await axios.get(
-			`http://localhost:5000/api/application/${param.id}`
+		const response = await fetch(
+			`http://localhost:5000/api/application/${param.id}`,
+			{ cache: "no-store" }
 		);
-		return response.data;
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
+};
+
+export const postAddComment = async (
+	param: Params,
+	data: IComments
+): Promise<IComments | null> => {
+	try {
+		return await axios.post(
+			`http://localhost:5000/api/comment/${param.id}`,
+			data,
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
 	} catch (error) {
 		console.log(error);
 		return null;
--- front-end/modules/ApplicationPostElement/index.tsx
+++ front-end/modules/ApplicationPostElement/index.tsx
@@ -1,55 +1,51 @@
 "use client";
 
 import imageUser from "@/public/icon/icon.jpg";
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { getIdApplication } from "./api/api";
 import { Comments } from "./components/Comments";
-import { IGetIdApplication } from "./type/type";
+import { FormComments } from "./components/FormComments";
+import style from "./post.module.scss";
 
 export const ApplicationPostElement = () => {
 	const param = useParams();
 
-	const [application, setApplication] = useState<IGetIdApplication | null>(
-		null
-	);
-	const [isLoading, setIdLoading] = useState<boolean>(true);
+	// const [application, setApplication] = useState<IGetIdApplication | null>(
+	// 	null
+	// );
+	// const [isLoadingState, setIdLoadingState] = useState<boolean>(true);
+
+	const { data, isLoading, isError, error } = useQuery({
+		queryKey: ["application", param.id],
+		queryFn: async () => getIdApplication(param),
+	});
 
 	useEffect(() => {
-		async function getDataId(id: Params) {
-			try {
-				const data = await getIdApplication(id);
-				setApplication(data);
-				setIdLoading(false);
-				return data;
-			} catch (error) {
-				console.log(error);
-				return null;
-			}
-		}
-		getDataId(param);
-	}, []);
+		console.log(data);
+	}, [data]);
 
 	return (
 		<>
 			{isLoading ? (
-				<div>
+				<div className={style.loading}>
 					<Link href='/applications'>Вернуться</Link> Loading...
 				</div>
 			) : (
 				<>
 					<Link href='/applications'>Вернуться</Link>
-					<div className='bg-gray-800 text-white rounded-xl shadow-lg mx-auto my-8 overflow-hidden w-full container'>
-						<div className='relative'>
-							<div className='relative h-40 bg-gradient-to-b from-purple-500 to-pink-400 flex justify-center items-center'>
-								<div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-24 h-24 rounded-full overflow-hidden border-4 border-pink-200 shadow-md z-10'>
+
+					<div className={style.application_post}>
+						<div className={style.wrapper}>
+							<div className={style.icon}>
+								<div className={style.image}>
 									<Image
 										className='rounded-full'
 										src={imageUser}
-										alt={`Avatar of ${application?.username}`}
+										alt={`Avatar of ${data?.username}`}
 										width={120}
 										height={120}
 										objectFit='cover'
@@ -59,23 +55,18 @@ export const ApplicationPostElement = () => {
 									/>
 								</div>
 							</div>
-							<div className='p-4 flex justify-center items-center'>
-								<h2 className='text-xl font-bold text-pink-300 ml-4'>
-									{application?.username}
-								</h2>
+							<div className={style.app_username}>
+								<h2 className={style.username}>{data?.username}</h2>
 							</div>
 						</div>
-						<div className='p-6 w-full'>
-							<h1 className='text-2xl font-extrabold tracking-tight mb-4 text-pink-200 text-center'>
-								{application?.title}
-							</h1>
-							<div className='text-gray-200 text-xl bg-gray-900 bg-opacity-30 p-4 rounded-lg shadow-md prose w-full whitespace-normal break-words'>
-								{application?.text}
-							</div>
+						<div className={style.title_text}>
+							<h1 className={style.title}>{data?.title}</h1>
+							<div className={`${style.text} prose`}>{data?.text}</div>
 						</div>
 					</div>
-					<div className='shadow-lg mx-auto my-8 overflow-hidden w-full container'>
-						<Comments comments={application?.comments} />
+					<div className={style.form_comments}>
+						<FormComments id={param} />
+						<Comments comments={data?.comments} />
 					</div>
 				</>
 			)}
